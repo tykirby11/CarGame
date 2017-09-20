76c677faf37b7bb5ba0c5e5d8abfd3d991a11289
tykirby11 <tyakirby@gmail.com>
2017-09-19 22:59:42 -0500

    First round

---
+++ Assets/Recources/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 10f;
+    public float rotationSpeed = 5f;
+    public float torque = -2f;
+
+
+    private Rigidbody2D theRigidbody;
+
+
+	// Use this for initialization
+	void Start () {
+        theRigidbody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float inputX = Input.GetAxis("Vertical");
+        float inputZ = Input.GetAxis("Horizontal");
+       // Debug.Log(inputX);
+        //theRigidbody.velocity = new Vector2(inputX * speed, theRigidbody.velocity.y);
+
+        //transform.Rotate(Vector3.forward, inputZ * rotationSpeed);
+
+        if (Input.GetButton("Vertical")) {
+            theRigidbody.AddForce(transform.up * speed);
+        }
+
+        theRigidbody.AddTorque(inputZ * torque);
+    }
+    
+}
---
+++ Assets/Recources/Scripts/Movement2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2 : MonoBehaviour {
+
+    float speed = 10f;
+    float torque = -2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (Input.GetButton("Vertical"))
+        {
+            rb.AddForce(transform.up * speed);
+        }
+
+        rb.AddTorque(Input.GetAxis("Horizontal") * torque);
+	}
+
+}
